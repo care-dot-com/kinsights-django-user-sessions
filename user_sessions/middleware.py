9e72f122bcb38bf7d1b0398519b6007093db6528
Ajay Katwe k <ajay.katwe@gmail.com>
2019-12-18 17:44:30 +0530

    :art: use http date instead of cookie_date

--- user_sessions/middleware.py
+++ user_sessions/middleware.py
@@ -2,7 +2,7 @@ import time
 
 from django.conf import settings
 from django.utils.cache import patch_vary_headers
-from django.utils.http import cookie_date
+from django.utils.http import http_date as cookie_date
 
 try:
     from importlib import import_module
